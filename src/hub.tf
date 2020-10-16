6e828705a57c908964ca4bd2226d7ed2f55a0e8e
Petrik van der Velde <petrikvandervelde@gmail.com>
2020-10-16 17:39:16 +1300

    Grant all the key-vault permissions to myself

--- src/hub.tf
+++ src/hub.tf
@@ -231,16 +231,70 @@ resource "azurerm_key_vault" "keys" {
     tenant_id = data.azurerm_client_config.current.tenant_id
     object_id = data.azurerm_client_config.current.object_id
 
-    key_permissions = [
+    certificate_permissions = [
+      "backup",
+      "create",
+      "delete",
+      "deleteissuers",
       "get",
+      "getissuers",
+      "import",
+      "list",
+      "listissuers",
+      "managecontacts",
+      "manageissuers",
+      "purge",
+      "recover",
+      "restore",
+      "setissuers",
+      "update",
+    ]
+
+    key_permissions = [
+      "backup",
+      "create",
+      "decrypt",
+      "delete",
+      "encrypt",
+      "get",
+      "import",
+      "list",
+      "purge",
+      "recover",
+      "restore",
+      "sign",
+      "unwrapKey",
+      "update",
+      "verify",
+      "wrapKey"
     ]
 
     secret_permissions = [
+      "backup",
+      "delete",
       "get",
+      "list",
+      "purge",
+      "recover",
+      "restore",
+      "set"
     ]
 
     storage_permissions = [
+      "backup",
+      "delete",
+      "deletesas",
       "get",
+      "getsas",
+      "list",
+      "listsas",
+      "purge",
+      "recover",
+      "regeneratekey",
+      "restore",
+      "set",
+      "setsas",
+      "update"
     ]
   }
 
